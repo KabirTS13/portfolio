a5f41117fafcebfb56350464f7d160044b6b6525
Kabir S <wonderts56@gmail.com>
2024-04-16 20:16:34 -0400

    Added all of the basic auto-generated react and firebase files

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCPJ8opIFdCa5kMW8mw6FQULq0lrXnuzA0",
+  authDomain: "ksportfolio-d0bd8.firebaseapp.com",
+  projectId: "ksportfolio-d0bd8",
+  storageBucket: "ksportfolio-d0bd8.appspot.com",
+  messagingSenderId: "960983253487",
+  appId: "1:960983253487:web:41d4afc539ca6200ceb61a"
+};
+
+// Initialize Firebase app
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+export { db };
