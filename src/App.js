2f4dffc4cd1d0757ffb81e14c4bc576406ac5b5f
Kabir S <wonderts56@gmail.com>
2024-05-10 15:08:31 -0400

    Added the most basic signin function just to test out if it works.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import TodoList from './todo.js'
 import Weather from './weather.js';
 import Header from './header.js';
 import Project from './projects.js';
 import Connections from './connections.js';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from './firebase';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+
+function GoogleSignIn() {
+    const signInWithGoogle = () => {
+        const provider = new GoogleAuthProvider();
+
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // You can access the signed-in user's information here
+                const user = result.user;
+                console.log(user);
+            })
+            .catch((error) => {
+                // Handle errors here
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.error(errorCode, errorMessage);
+            });
+    };
+
+    return (
+        <div>
+            <button onClick={signInWithGoogle}>Sign In with Google</button>
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <>
-      <Header />
-      <div className="flex justify-between">
-      <Connections />
-        <Weather />
-      </div>
-      <div className="flex justify-between">
-        <Project />
-        <TodoList />
-      </div>
+    const [user] = useAuthState(auth);
 
-
-    </>
-  );
+    return (
+        <>
+            {user ? (
+                <>
+                    <Header />
+                    <div className="flex justify-between">
+                        <Connections />
+                        <Weather />
+                    </div>
+                    <div className="flex justify-between">
+                        <Project />
+                        <TodoList />
+                    </div>
+                </>
+            ) : (
+                <GoogleSignIn />
+            )}
+        </>
+    );
 }
 
 export default App;
